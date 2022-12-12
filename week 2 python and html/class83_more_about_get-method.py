918c2443de8a8bbaa2ca0fb943513286d148d83d
pranjal singh <114800201+pranjalsingh2244@users.noreply.github.com>
2022-12-12 21:13:03 +0530

    Add files via upload

---
+++ week 2 python and html/class83_more_about_get-method.py
@@ -0,0 +1,6 @@
+user = {"name":"Sumit" , "age":"19"}
+print(user.get("age"))
+
+print(user.get("fav" , "not found"))
+
+## in this video we come to know that for printing of "none" what are some else we can prints
